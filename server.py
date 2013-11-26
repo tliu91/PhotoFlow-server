624ebcb743019497194787b24d77d91de83efdcb
Jeff Chan <jeffchan@mit.edu>
2013-11-26 13:34:29 -0500

    Load API keys from config file (requires config.py -- see your email)

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import twitter
 import forecast
 from flask import Flask, request, jsonify, make_response
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/streams', methods=['GET'])
 def streams():
@@ -16,13 +17,13 @@ def streams():
 # http://localhost:5000/search?lat=42.3595760&lon=-71.1020130&rad=2&q=MIT
 @app.route('/search', methods=['GET'])
 def tweet_search():
-	api = twitter.Twitter()
+	api = twitter.Twitter(app.config['TWITTER_API_KEY'], app.config['TWITTER_API_SECRET'])
 
 	query= request.args.get('q')
 	lat = request.args.get('lat')
 	lon = request.args.get('lon')
 	radius = request.args.get('rad')
-	
+
 	results = api.find_tweets(query, lat, lon, radius)
 	return make_response(jsonify(results), 404)
 
@@ -31,16 +32,16 @@ def tweet_search():
 # http://localhost:5000/forecast?lat=42.3595760&lon=-71.1020130&time=1385024400
 @app.route('/forecast', methods=['GET'])
 def forecast_search():
-	forecaster = forecast.ForecastIO()
+	forecaster = forecast.ForecastIO(app.config['FORECASTIO_API_KEY'])
 
 	lat = request.args.get('lat')
 	lon = request.args.get('lon')
 	time = request.args.get('time')
-	
+
 	results = forecaster.get_current_forecast(lat, lon, time)
 
 	return make_response(jsonify(results), 404)
-	
+
 @app.errorhandler(404)
 def not_found(error):
   	return make_response(jsonify({ 'error': 'Not found' }), 404)
