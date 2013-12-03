4badc19c6c279cf3a8f915973c5e944af376a6bc
Jeff Chan <jeffchan@mit.edu>
2013-12-03 02:28:47 -0500

    Add basic optparser to download script

--- download.rb
+++ download.rb
@@ -1,8 +1,36 @@
+#!/usr/bin/env ruby
 require 'open-uri'
 require 'fileutils'
+require 'optparse'
 
-bbox = true
-city = 'la'
+options = {}
+optparser = OptionParser.new do |opt|
+	opt.banner = "Usage: download.rb city [options]"
+
+	options[:skipto] = nil
+	opt.on("-s", "--skipto filename", "which filename to skip to") do |filename|
+		options[:skipto] = filename
+	end
+
+	options[:exact] = false
+	opt.on("-e", "--exact", "use exact lat/long, default to bounding box") do
+		options[:exact] = true
+	end
+
+	opt.on("-h", "--help", "help") do
+		puts optparser
+		exit
+	end
+end
+optparser.parse!
+
+bbox = !options[:exact]
+last_filename = options[:skipto]
+city = ARGV[0]
+
+if city.nil?
+	abort("Must specify a city.")
+end
 
 base_folder = "data"
 image_base_folder = "images"
@@ -16,7 +44,7 @@ end
 
 start_date = ''
 end_date = ''
-seen_last = true # HACK: set to false if you want to skip until last_filename is scanned
+seen_last = last_filename.nil?
 
 File.open("#{base_folder}/#{folder}/#{city}_data.txt", 'r').each_line do |line|
 	unless /^http/.match(line)
@@ -29,14 +57,13 @@ File.open("#{base_folder}/#{folder}/#{city}_data.txt", 'r').each_line do |line|
 		create_dir(dir)
 
 		filename = /[A-Za-z\d_]+\.jpg/.match(line)[0]
-		last_filename = '8857329604_a11fcedac7.jpg' 
-		if filename.eql? last_filename
+		if not seen_last and filename.eql? last_filename
 			seen_last = true
 		end
-		
+
 		if seen_last
 		  File.open(dir + "/#{filename}", 'wb') do |fo|
-		    begin	
+		    begin
 		      fo.write(open(line).read)
 		    rescue RuntimeError => e
 		      next
@@ -44,6 +71,6 @@ File.open("#{base_folder}/#{folder}/#{city}_data.txt", 'r').each_line do |line|
 		      next
 	 	    end
 		  end
-		end 
+		end
 	end
 end
