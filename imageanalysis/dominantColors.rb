0ed079cdf2fbb4e4c1184fed1bceceed83784258
Fidel Sosa <fidel@fidelsosa.com>
2013-12-03 02:32:32 -0500

    Added examples showing quantization and example color histograms

--- imageanalysis/dominantColors.rb
+++ imageanalysis/dominantColors.rb
@@ -1,24 +1,17 @@
 require 'RMagick'
  
-TOP_N = 10      # Number of swatches
+TOP_N = 256   # Number of swatches
  
 # Create a 1-row image that has a column for every color in the quantized
 # image. The columns are sorted by decreasing frequency of appearance in the
 # quantized image.
 def sort_by_decreasing_frequency(img)
   hist = img.color_histogram
-  puts hist.keys[0].to_hsla
   # sort by decreasing frequency
   sorted = hist.keys.sort_by {|p| -hist[p]}
   sorted = sorted * 100
   new_img = Magick::Image.new(hist.size, 100)
   new_img.store_pixels(0, 0, hist.size, 100, sorted)
-end
- 
-def get_pix(img)
-  palette = Magick::ImageList.new
-  pixels = img.get_pixels(0, 0, img.columns, 1)
-  # img.display
 end
 
 def average_color(quantized)
@@ -45,18 +38,26 @@ def average_color(quantized)
   canvas.rectangle(0, 0, 100, 100)
   canvas.draw(avg_image)
 
-  # avg_image.display
-
+  avg_image
 end
  
 original = Magick::Image.read("/Users/fsosa/Dev/PhotoFlow-server/images/lat-long-bbox/la/2011-03-01/5608490071_37ac2fef3b.jpg").first
-# original.display
  
 # reduce number of colors
 quantized = original.quantize(TOP_N, Magick::HSLColorspace)
- 
 
 # Create an image that has 1 pixel for each of the TOP_N colors.
 normal = sort_by_decreasing_frequency(quantized)
 avg_color = average_color(quantized)
-get_pix(normal)
+
+original.write("original.png")
+quantized.write("quantized_#{TOP_N}.png")
+normal.write("normalized_#{TOP_N}.png")
+avg_color.write("avg_color_#{TOP_N}.png")
+
+
+
+
+
+
+
