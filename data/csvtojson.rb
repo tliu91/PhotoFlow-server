28fea03cc07c215e4397bb26db2dc786bf6cd4e8
Jeff Chan <jeffchan@mit.edu>
2013-12-07 16:08:15 -0500

    Add interesting points

---
+++ data/csvtojson.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'csv'
+require 'json'
+
+options = {}
+optparser = OptionParser.new do |opt|
+	opt.banner = "Usage: csvtojson.rb [options]"
+
+	options[:input] = nil
+	opt.on("-i", "--input filename", "which filename to pull input from") do |filename|
+		options[:input] = filename
+	end
+
+	options[:output] = nil
+	opt.on("-o", "--output filename", "which filename to output to") do |filename|
+		options[:output] = filename
+	end
+
+	opt.on("-h", "--help", "help") do
+		puts optparser
+		exit
+	end
+end
+optparser.parse!
+
+exit if options[:input].nil?
+
+locations = []
+CSV.foreach(options[:input], :headers => true) do |row|
+	row['TYPE'].strip!
+	type = (row['TYPE'] == '<Null>' or row['TYPE'].empty?) ? '' : row['TYPE']
+	locations.push({
+		:name => row['NAME'],
+		:loc => [row['LONG'], row['LAT']],
+		:type => type
+	})
+end
+
+unless options[:output].nil?
+	json = JSON.pretty_generate(locations, :indent => '  ')
+	file = File.open(options[:output], 'w')
+	file.write(json)
+end
